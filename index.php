eca4c9aa92cc79a5701e578f4dfae75162e23007
indigoDusk <adams.dahlia@student.greenriver.edu>
2023-05-10 13:29:19 -0400

    set up links to tutorial and login

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 
 ini_set('display_errors',1);
 error_reporting(E_ALL);
@@ -15,6 +15,18 @@ $f3 -> route('GET /', function(){
     //display view page
     $view = new Template();
     echo $view->render('views/game.html');
+});
+
+//login route
+$f3 -> route('GET /login', function(){
+        $view = new Template();
+    echo $view->render('views/login.html');
+});
+
+//home route
+$f3 -> route('GET /how-to-play', function(){
+    $view = new Template();
+    echo $view->render('views/tutorial.html');
 });
 
 $f3->run();
