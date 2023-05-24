523c9ad61af3fa087d49bccd90aad8e415b766c1
David Kurilla <kurilla.david@student.greenriver.edu>
2023-05-24 14:27:16 -0400

    Configured and wrote documentation for SuperUser and Dice classes

--- model/domain/superuser.php
+++ model/domain/superuser.php
@@ -1,10 +1,20 @@
 <?php
 
+/**
+ * This class represents a SuperUser.
+ * @author David Kurilla
+ */
 class SuperUser extends User
 {
 
     private string $_email;
 
+    /**
+     * This is the constructor for the SuperUser.
+     * @param string $username the username of the SuperUser
+     * @param string $password the password of the SuperUser
+     * @param string $email the email of the SuperUser
+     */
     function __construct(string $username, string $password, string $email)
     {
         parent::__construct($username, $password);
@@ -12,11 +22,19 @@ class SuperUser extends User
         $this->_email = $email;
     }
 
+    /**
+     * This method gets the email of the SuperUser.
+     * @return string the email of the SuperUser
+     */
     function getEmail(): string
     {
         return $this->_email;
     }
 
+    /**
+     * This method sets the email of the SuperUser.
+     * @param string $email the email of the SuperUser
+     */
     function setEmail(string $email): void
     {
         $this->_email = $email;
