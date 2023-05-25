b9f3a6e6e2292cc9e3880cde79ce617a9dd6e95e
David Kurilla <kurilla.david@student.greenriver.edu>
2023-05-25 12:16:23 -0400

    Created Validations class and validate script

---
+++ model/domain/validations.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Class provides static methods to validate inputs
+ * @author David Kurilla
+ */
+class Validations
+{
+
+    /**
+     * This method validates the username of the User or SuperUser
+     * @param string $username the username of the User or SuperUser
+     * @return bool the validity of the username of the User or SuperUser
+     */
+    static function validateUsername(string $username): bool
+    {
+        if (preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $username)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * This method validates the email of the SuperUser
+     * @param string $email the email address of the SuperUser
+     * @return bool the validity of the email address of the SuperUser
+     */
+    static function validateEmail(string $email): bool
+    {
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
+}
